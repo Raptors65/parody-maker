ee6416c12df911fea61a85c64214d3a2018fe317
Lucas Kim <lucaskamadakim@gmail.com>
2022-08-23 21:06:33 -0400

    Fix image error when searching for some songs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.genius.com"],
+    domains: ["images.genius.com", "images.rapgenius.com"],
   },
 };
 
